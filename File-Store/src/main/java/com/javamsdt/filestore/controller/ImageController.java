0352b55d0ed8700ee2a86950f19a9f0b89138dfe
Ahmed Samy <serenitydiver@hotmail.com>
2023-01-15 21:08:47 +0100

    Focus on store images to DB

--- File-Store/src/main/java/com/javamsdt/filestore/controller/ImageController.java
+++ File-Store/src/main/java/com/javamsdt/filestore/controller/ImageController.java
@@ -1,16 +1,13 @@
 package com.javamsdt.filestore.controller;
 
-import com.javamsdt.filestore.dto.ImageDto;
 import com.javamsdt.filestore.model.Image;
 import com.javamsdt.filestore.service.ImageToDbService;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.servlet.ServletContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.CacheControl;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -29,63 +26,51 @@ public class ImageController {
 
   private final ImageToDbService imageToDbService;
 
-  private final ServletContext servletContext;
-
   @PostMapping
-  Long uploadImage(@RequestParam("image") MultipartFile multipartImage) throws Exception {
+  Long saveImage(@RequestParam("image") MultipartFile multipartImage) throws Exception {
     return imageToDbService.saveImageReturnId(multipartImage);
   }
 
+  @PostMapping("/images")
+  List<Long> saveImages(@RequestParam("images") MultipartFile[] multipartImage) throws Exception {
+    List<MultipartFile> multipartFiles = Arrays.stream(multipartImage).collect(Collectors.toList());
+    System.out.println("multipartImage.size():: " + multipartFiles.size());
+    return imageToDbService.saveImages(multipartFiles);
+  }
+
   @GetMapping(value = "/{imageId}", produces = MediaType.IMAGE_JPEG_VALUE)
-  Resource downloadImage(@PathVariable Long imageId) {
+  Resource findImageById(@PathVariable Long imageId) {
     byte[] image = imageToDbService.getImageById(imageId)
       .getContent();
     return new ByteArrayResource(image);
   }
 
-  @GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
-  List<Resource> downloadImage(@RequestParam("ids") List<Long> ids) {
-    return imageToDbService.findByImageIds(ids)
+  @GetMapping
+  ResponseEntity<List<Resource>> findImagesByIdsResource(@RequestParam("ids") List<Long> ids) {
+    List<Resource> resources = imageToDbService.findByImageIds(ids)
       .stream()
       .peek(image -> System.out.println(image.getName()))
       .map(Image::getContent)
       .map(ByteArrayResource::new)
       .collect(Collectors.toList());
+    return new ResponseEntity<>(resources, HttpStatus.OK);
+  }
+
+  @GetMapping("/allImages")
+  ResponseEntity<List<Image>> findImagesByIds(@RequestParam("ids") List<Long> ids) {
+/*    List<Resource> resources = imageToDbService.findByImageIds(ids)
+      .stream()
+      .peek(image -> System.out.println(image.getName()))
+      .map(Image::getContent)
+      .map(ByteArrayResource::new)
+      .collect(Collectors.toList())*/;
+    return new ResponseEntity<>(imageToDbService.findByImageIds(ids), HttpStatus.OK);
   }
 
   @GetMapping(value = "/image-name/{name}", produces = MediaType.IMAGE_JPEG_VALUE)
-  Resource downloadImageByName(@PathVariable("name") String name) {
+  Resource findImageByName(@PathVariable("name") String name) {
     byte[] image = imageToDbService.findImageByName(name)
       .getContent();
     return new ByteArrayResource(image);
-  }
-
-  @GetMapping(value = "/image/{imageId}", produces = MediaType.IMAGE_JPEG_VALUE)
-  ImageDto downloadImageById(@PathVariable Long imageId) {
-    return imageToDbService.downloadImageById(imageId);
-  }
-
-  @GetMapping(value = "/image-resource/{imageId}")
-  public ResponseEntity<Resource> getImageAsResource(@PathVariable Long imageId) {
-    Image image = imageToDbService.getImageById(imageId);
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.add("imageName", image.getName());
-    Resource resource = new ByteArrayResource(image.getContent());
-    // new ServletContextResource(servletContext, "/WEB-INF/images/image-example.jpg");
-    return new ResponseEntity<>(resource, headers, HttpStatus.OK);
-  }
-
-  @GetMapping(value = "/image-response-entity/{imageId}")
-  public ResponseEntity<byte[]> getImageAsResponseEntity(@PathVariable Long imageId) {
-    Image image = imageToDbService.getImageById(imageId);
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.add("imageName", image.getName());
-    // InputStream in = servletContext.getResourceAsStream("/WEB-INF/images/image-example.jpg");
-    // byte[] media = IOUtils.toByteArray(in);
-    headers.setCacheControl(CacheControl.noCache().getHeaderValue());
-
-    return new ResponseEntity<>(image.getContent(), headers, HttpStatus.OK);
   }
 }
--- File-Store/src/main/java/com/javamsdt/filestore/service/ImageToDbService.java
+++ File-Store/src/main/java/com/javamsdt/filestore/service/ImageToDbService.java
@@ -4,8 +4,9 @@ import com.javamsdt.filestore.dto.ImageDto;
 import com.javamsdt.filestore.mapper.ImageMapper;
 import com.javamsdt.filestore.model.Image;
 import com.javamsdt.filestore.repository.ImageRepository;
+import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -20,8 +21,6 @@ public class ImageToDbService {
   private final ImageMapper imageMapper;
 
   public Long saveImageReturnId(MultipartFile multipartImage) throws Exception {
-    System.out.println(multipartImage.getName());
-    System.out.println(extractImageName(Objects.requireNonNull(multipartImage.getOriginalFilename())));
     Image dbImage = new Image();
     dbImage.setName(extractImageName(multipartImage.getOriginalFilename()));
     dbImage.setContent(multipartImage.getBytes());
@@ -30,10 +29,18 @@ public class ImageToDbService {
       .getId();
   }
 
+  public List<Long> saveImages(List<MultipartFile> multipartFiles) {
+    return imageRepository.saveAll(toImages(multipartFiles))
+      .stream()
+      .map(Image::getId)
+      .collect(Collectors.toList());
+  }
+
   public Long saveImageReturnIdFromImage(Image image) {
     return imageRepository.save(image)
       .getId();
   }
+
   public Image getImageById(Long imageId) {
     return imageRepository.findById(imageId)
       .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
@@ -55,6 +62,21 @@ public class ImageToDbService {
   private String extractImageName(String imagePath) {
     int lastDot = imagePath.lastIndexOf(".");
     return imagePath.substring(0, lastDot);
+  }
+
+  private List<Image> toImages(List<MultipartFile> multipartFiles) {
+    return multipartFiles.stream()
+      .map(fileImage -> {
+        Image image = new Image();
+        image.setName(extractImageName(fileImage.getOriginalFilename()));
+        try {
+          image.setContent(fileImage.getBytes());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+        return image;
+      })
+      .collect(Collectors.toList());
   }
 
 }
