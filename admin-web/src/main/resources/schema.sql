64828688331fa51bbbe936e56fdd922d48b76110
Ahmed Samy <serenitydiver@hotmail.com>
2023-01-29 13:55:18 +0100

    Adding JDBC Authintication and BCrypt Encoder

--- admin-web/src/main/resources/schema.sql
+++ admin-web/src/main/resources/schema.sql
@@ -12,3 +12,17 @@ CREATE TABLE orders(
     order_info varchar (2048) not null,
     foreign key (customer_id) references customers(customer_id)
 );
+
+CREATE TABLE users (
+    username VARCHAR(50) NOT NULL PRIMARY KEY,
+    password VARCHAR(500) NOT NULL,
+    enabled boolean
+);
+
+CREATE TABLE authorities (
+    username VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    constraint fk_authorities_users FOREIGN KEY (username) REFERENCES users(username)
+);
+
+-- CREATE UNIQUE INDEX ix_auth-username ON authorities (username, authority);
