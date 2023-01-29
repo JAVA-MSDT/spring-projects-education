93b1340a2a02efe713c99d6d39ab7758721a10b5
Ahmed Samy <serenitydiver@hotmail.com>
2023-01-29 13:12:09 +0100

    Initializing Project

---
+++ admin-web/src/main/java/com/springsecurity/data/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.springsecurity.data.repository;
+
+import com.springsecurity.data.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+}
---
+++ admin-web/src/main/java/com/springsecurity/data/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.springsecurity.data.repository;
+
+import com.springsecurity.data.model.Order;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends CrudRepository<Order, Long> {
+    Iterable<Order> findAllByCustomerId(long customerId);
+}
