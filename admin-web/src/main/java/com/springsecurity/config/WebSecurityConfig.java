a2278066f5ef234d34d9210cef751852b6a90f20
Ahmed Samy <serenitydiver@hotmail.com>
2023-01-30 14:18:23 +0100

    Adding LDAB

--- admin-web/src/main/java/com/springsecurity/config/WebSecurityConfig.java
+++ admin-web/src/main/java/com/springsecurity/config/WebSecurityConfig.java
@@ -1,43 +1,60 @@
 package com.springsecurity.config;
 
 import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-      .authorizeRequests()
-      .antMatchers("/", "/home").permitAll()
-      .antMatchers("/customers/**").hasRole("USER")
-      .antMatchers("/v1/customers/**").hasAnyRole("USER", "ADMIN")
-      .antMatchers("/orders/**").hasRole("ADMIN")
-      .anyRequest().authenticated()
-      .and()
-      .httpBasic();
-  }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/customers/**").hasRole("USER")
+                .antMatchers("/v1/customers/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/orders/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
 
-  @Bean
-  public UserDetailsService users(DataSource dataSource) {
-    return new JdbcUserDetailsManager(dataSource);
-  }
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=landon,dc=org")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .passwordAttribute("userPassword");
+    }
 
-  @Bean
-  public GrantedAuthoritiesMapper authoritiesMapper() {
-    SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
-    authorityMapper.setConvertToUpperCase(true);
-    return authorityMapper;
-  }
+    @Bean
+    public UserDetailsService users(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public GrantedAuthoritiesMapper authoritiesMapper() {
+        SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
+        authorityMapper.setConvertToUpperCase(true);
+        return authorityMapper;
+    }
 
 }
