db1ee3eb32a67f0afe05a24bdd676337b7ceced6
Ahmed Samy <serenitydiver@hotmail.com>
2023-07-03 10:51:45 +0200

    Intializing the main projects

---
+++ 01-Song_Resource_Service/resourcemedata/src/main/java/com/javamsdt/resourcemedata/ResourceMetadataApplication.java
@@ -0,0 +1,13 @@
+package com.javamsdt.resourcemedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceMetadataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceMetadataApplication.class, args);
+	}
+
+}
---
+++ 01-Song_Resource_Service/resourcemedata/src/test/java/com/javamsdt/resourcemedata/ResourceMetadataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javamsdt.resourcemedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceMetadataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
