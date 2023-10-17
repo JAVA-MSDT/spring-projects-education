826ac404dc3ca456d982aca1648e3a4bec08aff4
Ahmed Samy <serenitydiver@hotmail.com>
2023-10-17 23:18:40 +0200

    Refactoring Image controller, service and validation

--- File-Store/src/test/java/com/javamsdt/filestore/util/DataProvider.java
+++
@@ -1,2 +0,0 @@
-package com.javamsdt.filestore.util;public class DataProvider {
-}
--- file-store-front/src/domain/image.ts
+++
@@ -1,8 +0,0 @@
-export interface Image {
-    id?: string;
-    content: string | null;
-    name: string;
-    location: string;
-    alt: string;
-    extension: string;
-}
