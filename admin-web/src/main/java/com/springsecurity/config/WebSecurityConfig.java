cdae5ada920ce6db675f9a2366db025ff9c53a5e
Ahmed Samy <serenitydiver@hotmail.com>
2024-12-25 09:47:27 +0100

    Updgrading to use java 21 and spring boot 3.4.1

--- admin-web/src/main/java/com/springsecurity/config/WebSecurityConfig.java
+++ admin-web/src/main/java/com/springsecurity/config/WebSecurityConfig.java
@@ -1,43 +1,50 @@
 package com.springsecurity.config;
 
-import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-      .authorizeRequests()
-      .antMatchers("/", "/home").permitAll()
-      .antMatchers("/customers/**").hasRole("USER")
-      .antMatchers("/v1/customers/**").hasAnyRole("USER", "ADMIN")
-      .antMatchers("/orders/**").hasRole("ADMIN")
-      .anyRequest().authenticated()
-      .and()
-      .httpBasic();
-  }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(
+                        authorize -> {
+                            authorize.anyRequest().permitAll();
+                        }
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
 
-  @Bean
-  public UserDetailsService users(DataSource dataSource) {
-    return new JdbcUserDetailsManager(dataSource);
-  }
+    /// /      .antMatchers("/", "/home").permitAll()
+    /// /      .antMatchers("/customers/**").hasRole("USER")
+    /// /      .antMatchers("/v1/customers/**").hasAnyRole("USER", "ADMIN")
+    /// /      .antMatchers("/orders/**").hasRole("ADMIN")
+    /// /      .anyRequest().authenticated()
 
-  @Bean
-  public GrantedAuthoritiesMapper authoritiesMapper() {
-    SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
-    authorityMapper.setConvertToUpperCase(true);
-    return authorityMapper;
-  }
+    @Bean
+    public UserDetailsService users(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public GrantedAuthoritiesMapper authoritiesMapper() {
+        SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
+        authorityMapper.setConvertToUpperCase(true);
+        return authorityMapper;
+    }
 
 }
--- admin-web/src/main/java/com/springsecurity/data/model/Order.java
+++ admin-web/src/main/java/com/springsecurity/data/model/Order.java
@@ -1,11 +1,5 @@
 package com.springsecurity.data.model;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="orders")
