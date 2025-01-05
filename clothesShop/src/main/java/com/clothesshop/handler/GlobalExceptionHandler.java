11931de8a6d33265b9ccdda05aa0c0447a32f8cc
Ahmed Samy <serenitydiver@hotmail.com>
2025-01-05 13:37:26 +0100

    Adding Exception controller advice to handle exceptions

---
+++ clothesShop/src/main/java/com/clothesshop/handler/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.clothesshop.handler;
+
+import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    public static final String ERROR_PAGE = "shared/error";
+    // https://pandac.in/blogs/thymeleaf-errorhandle/
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception ex, Model model) {
+        if(ex instanceof ResponseStatusException) {
+            model.addAttribute("errorMessage", ex.getLocalizedMessage());
+            return ERROR_PAGE;
+        }
+
+        if(ex instanceof AuthorizationDeniedException) {
+            model.addAttribute("errorMessage", ex.getLocalizedMessage());
+            return ERROR_PAGE;
+        }
+        model.addAttribute("errorMessage", ex.getLocalizedMessage());
+        return ERROR_PAGE;
+    }
+}
